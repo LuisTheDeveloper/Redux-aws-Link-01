25aad39800fb5191755d75c79bebce63df021e37
Luis <sprintlmaga@gmail.com>
2020-03-21 15:11:07 +0000

    add middleware

--- index.js
+++ index.js
@@ -1,21 +1,56 @@
 import C from './constants'
-import appReducer from './store/reducers'
-import { createStore } from 'redux'
 
-const store = createStore(appReducer)
+// import our function that we use to create stores - storeFactory is an optional name you can call whatever you want
+// there is an index file under store folder all we need is to add the folder to the import command line.
+import storeFactory from './store'
 
-const unsubscribeGoalLogger = store.subscribe(
-	() => console.log(' Goal: ${store.getState().goal}')
-)
+// load initialState from local storage: to include on the store we create
+const initialState = (localStorage['redux-store']) ?
+	JSON.parse(localStorage['redux-store']) :
+	{}
 
-setInterval(() => {
+// save state to localStorage: to include on the store we create
+const saveState = () => {
+	const state = JSON.stringify(store.getState())
+	localStorage['redux-store'] = state 
+}
 
-	store.dispatch({
-		type: C.SET_GOAL,
-		payload: Math.floor(Math.random() * 100)
-	})
-}, 250)
+// We can create a new store based on our storeFactory, optional initialState
+// load initialStage from localstorage
+const store = storeFactory(initialState)
 
-setTimeout(() => {
-	unsubscribeGoalLogger();
-}, 3000)
+// Now that we have a store we can use our subscribe method to our store
+// subscribe the savestate method to our store
+// every time we dispatch an action it will save our state 
+// the store has MIDDLEWARE associated with it will log console groups for each action that's being dispatched
+store.subscribe(saveState)
+
+store.dispatch({
+	type: C.ADD_DAY,
+	payload: {
+		"resort": "Mt Shasta",
+		"date": "2016-10-28",
+		"powder": true,
+		"backcountry": true	
+	}
+})
+
+store.dispatch({
+	type: C.ADD_DAY,
+	payload: {
+		"resort": "Mt Everest",
+		"date": "2016-3-28",
+		"powder": true,
+		"backcountry": false	
+	}
+})
+
+store.dispatch({
+	type: C.ADD_DAY,
+	payload: {
+		"resort": "Lion Sierra",
+		"date": "2016-12-15",
+		"powder": false,
+		"backcountry": true	
+	}
+})
--- store/index.js
+++ store/index.js
@@ -1,6 +1,6 @@
 import C from '../constants'
 import appReducer from './reducers'
-import { createStore } from 'redux'
+import { createStore, applyMiddleware } from 'redux'
 
 /*  Index.js exports a function that we use to create stores */
 
@@ -17,25 +17,66 @@ import { createStore } from 'redux'
 
 /* Middleware for logging messages to the console */                                    
 
-// the function 1: gets a specific argument: the store will be injected into this function
+// the function consoleMessages: gets a specific argument: the store will be injected into this function
 // we must return a function and a different argument will be injected into 
 //  the function 2
 //  that argument is called next and the function 2 will be used to invoke or dispatch the action
 //  function 2 need to return another function, function 3
 //      function 3 will pass the action itself - action that is going to be dispatched.
 //
-// **** using arrow functions to make the code nicer **** :)
+/* **** using arrow functions to make the code nicer **** :)
+// **** instead of having this:
 const consoleMessages = function(store){
     return function(next){
-        return function(action)
+        return function(action){
+
+        }
     }
 }
+*/
 
+// we have this: 3 arrow functions in a row and without parentheses because they only have one argument
+// This function give us the action that is currently being dispatched, along with a mechanism to
+// dispatch that action and change the state
+// we make sure that we do not break the store's current dispatch pipeline
+const consoleMessages = store => next => action => {
+
+    let result
+
+    // Functionality before we dispatch the action
+    // Console groups allows us to group all of the logs that are associated with this action into a collapsible group on the console
+    console.groupCollapsed(`dispatching action => ${action.type}`)  // string template to log the action that is being dispatched
+    
+    console.log('ski days', store.getState().allSkiDays.length) // information about the state before the action is dispatched
+
+    result = next(action)   // where the action gets dispatched and our state will change we can add functionality before or after this line.
+
+    // get state after the action has been dispatched
+    let { allSkiDays, goal, errors, resortNames } = store.getState()
+
+    console.log(`
+
+        ski days: ${allSkiDays.length}
+        goal: ${goal}
+        fetching: ${resortNames.fetching}
+        suggestions: ${resortNames.suggestions}
+        errors: ${errors.length}
+    
+    `)
+
+    console.groupEnd()
+
+    return result           // to make sure the state change gets registered we must return the result
+}
 
 // function to use our createStore with our appReducer
 // function must be able to take in the initialState
 // initialState is set to an empty object
 // initialState is to be added to our createStore method
 export default (initialState={}) => {
-    return createStore(appREducer, initialState)
+    // applyMiddleware is going to apply our consoleMessages 
+    // and return a function. 
+    // We send to that function the createStore function that we are using to build our stores
+    // this will return a new createStore function, that will create stores with our consoleMessages middleware
+    return applyMiddleware(consoleMessages) (createStore) (appReducer, initialState)
 }
